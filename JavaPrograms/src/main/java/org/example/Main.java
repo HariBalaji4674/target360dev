da9effe18e1a28da2913f4ea23ec80f4757b8198
Hari Vardhan Reddy Peddireddy <Harivardhanreddy.Peddireddy@blueyonder.com>
2024-05-28 21:46:01 +0530

    28-May-2024

--- JavaPrograms/src/main/java/org/example/Main.java
+++ JavaPrograms/src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Main{
-    public static void main(String[] Args) {
+    public static void main(String...Args) {
         System.out.println("Peddireddy Hari Vardhan Reddy");
     }
 
--- JavaPrograms/src/main/java/org/example/blue.java
+++ JavaPrograms/src/main/java/org/example/blue.java
@@ -1,9 +1,28 @@
 package org.example;
 
-public class blue {
-  public static void main(String[] args) {
-    System.out.println("Peddireddy Hari Vardhan reddy");
-    // System.out.println("Peddireddy Hari Vardhan Reddy");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
+public class Blue {
+  public static void main(String[] args) throws IOException {
+
+    Scanner sc = new Scanner(System.in);
+    System.err.print("Please Enter your name : ");
+    String name = sc.nextLine();
+    System.out.print("Please enter a Number");
+    int number = sc.nextInt();
+    System.out.println("This is a number "+ number);
+    System.out.println("My name is "+ name);
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    String name1 = reader.readLine();
+    System.out.println("Name "+ name1 );
+    System.out.println(method1(20));
+  }
+  static int method1(int number){
+    return number;
   }
 }
